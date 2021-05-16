0d63e6f0dc0219150d71f0c8b11dbb8f25ee9249
git-ethan-mars <dima.nesterov.2002@inbox.ru>
2021-05-16 22:13:40 +0500

    Added menu

--- players.py
+++ players.py
@@ -65,54 +65,78 @@ class Computer:
                 return True
         return False
 
-    def shoot(self, player) -> (
+    def shoot(self, game) -> (
             int, int, int, int, bool, tuple or None, tuple or None):
         is_horizontal = True
         is_vertical = True
         first_ship = None
         last_ship = None
-        if self.possible_targets:
-            x, y = random.choice(self.possible_targets)
-            self.possible_targets.remove((x, y))
-        else:
+        if not game.is_smart:
             x, y = random.choice(self.free_cells)
-        self.free_cells.remove((x, y))
-        if (x, y) in [j for i in player.ships.ships_set for j in i]:
-            self.last_hurt_ship.append((x, y))
-            if len(self.last_hurt_ship) >= 2:
-                if self.last_hurt_ship[0][0] == self.last_hurt_ship[1][0]:
-                    is_horizontal = False
-                    for i in range(len(self.possible_targets) - 1, -1, -1):
-                        if self.possible_targets[i][0] != self.last_hurt_ship[0][0]:
-                            self.free_cells.remove(self.possible_targets[i])
-                            del self.possible_targets[i]
-                if self.last_hurt_ship[0][1] == self.last_hurt_ship[1][1]:
-                    is_vertical = False
-                    for i in range(len(self.possible_targets) - 1, -1, -1):
-                        if self.possible_targets[i][1] != self.last_hurt_ship[0][1]:
-                            self.free_cells.remove(self.possible_targets[i])
-                            del self.possible_targets[i]
-            if ships.Ships.is_on_field((x - 1, y)) and (x - 1, y) in self.free_cells \
-                    and is_horizontal:
-                self.possible_targets.append((x - 1, y))
-            if ships.Ships.is_on_field((x + 1, y)) and (x + 1, y) in self.free_cells \
-                    and is_horizontal:
-                self.possible_targets.append((x + 1, y))
-            if ships.Ships.is_on_field((x, y - 1)) and (x, y - 1) in self.free_cells \
-                    and is_vertical:
-                self.possible_targets.append((x, y - 1))
-            if ships.Ships.is_on_field((x, y + 1)) and (x, y + 1) in self.free_cells \
-                    and is_vertical:
-                self.possible_targets.append((x, y + 1))
-        if self.is_destroyed(player):
-            self.last_hurt_ship.sort()
-            first_ship = self.last_hurt_ship[0]
-            last_ship = self.last_hurt_ship[-1]
-            for ship in self.possible_targets:
-                self.free_cells.remove(ship)
-            self.last_hurt_ship = []
-            self.possible_targets = []
+            self.free_cells.remove((x, y))
+            if (x, y) in [j for i in game.player.ships.ships_set for j in i]:
+                self.last_hurt_ship.append((x, y))
+            if self.is_destroyed(game.computer):
+                ship_candidate = []
+                for ship in game.computer.ships.ships_set:
+                    if set(ship) <= set(self.last_hurt_ship):
+                        ship_candidate = ship
+                        break
+                temp = []
+                for cell in ship_candidate:
+                    if cell in self.last_hurt_ship:
+                        temp.append(cell)
+                        self.last_hurt_ship.remove(cell)
+                temp.sort()
+                first_ship = temp[0]
+                last_ship = temp[-1]
+        else:
+            if self.possible_targets:
+                x, y = random.choice(self.possible_targets)
+                self.possible_targets.remove((x, y))
+            else:
+                x, y = random.choice(self.free_cells)
+            self.free_cells.remove((x, y))
+            if (x, y) in [j for i in game.player.ships.ships_set for j in i]:
+                self.last_hurt_ship.append((x, y))
+                if len(self.last_hurt_ship) >= 2:
+                    if self.last_hurt_ship[0][0] == self.last_hurt_ship[1][0]:
+                        is_horizontal = False
+                        for i in range(len(self.possible_targets) - 1, -1, -1):
+                            if self.possible_targets[i][0] != self.last_hurt_ship[0][0]:
+                                self.free_cells.remove(self.possible_targets[i])
+                                del self.possible_targets[i]
+                    if self.last_hurt_ship[0][1] == self.last_hurt_ship[1][1]:
+                        is_vertical = False
+                        for i in range(len(self.possible_targets) - 1, -1, -1):
+                            if self.possible_targets[i][1] != self.last_hurt_ship[0][1]:
+                                self.free_cells.remove(self.possible_targets[i])
+                                del self.possible_targets[i]
+                if ships.Ships.is_on_field((x - 1, y)) and (
+                        x - 1, y) in self.free_cells \
+                        and is_horizontal:
+                    self.possible_targets.append((x - 1, y))
+                if ships.Ships.is_on_field((x + 1, y)) and (
+                        x + 1, y) in self.free_cells \
+                        and is_horizontal:
+                    self.possible_targets.append((x + 1, y))
+                if ships.Ships.is_on_field((x, y - 1)) and (
+                        x, y - 1) in self.free_cells \
+                        and is_vertical:
+                    self.possible_targets.append((x, y - 1))
+                if ships.Ships.is_on_field((x, y + 1)) and (
+                        x, y + 1) in self.free_cells \
+                        and is_vertical:
+                    self.possible_targets.append((x, y + 1))
+            if self.is_destroyed(game.player):
+                self.last_hurt_ship.sort()
+                first_ship = self.last_hurt_ship[0]
+                last_ship = self.last_hurt_ship[-1]
+                for ship in self.possible_targets:
+                    self.free_cells.remove(ship)
+                self.last_hurt_ship = []
+                self.possible_targets = []
         return x, y, self.enemy_left, self.enemy_top, (x, y) in [j for i in
-                                                                 player.ships.ships_set
+                                                                 game.player.ships.ships_set
                                                                  for j in
                                                                  i], first_ship, last_ship
